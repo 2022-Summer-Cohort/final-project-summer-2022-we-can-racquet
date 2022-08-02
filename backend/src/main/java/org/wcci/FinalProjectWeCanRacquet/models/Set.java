23f57d136ec5f2324ddd7d10b1e5bac03d42dc33
tastyCanOfMalk <eddytheflow@gmail.com>
2022-08-02 15:16:43 -0400

    add navbar image

---
+++ backend/src/main/java/org/wcci/FinalProjectWeCanRacquet/models/Set.java
@@ -0,0 +1,2 @@
+package org.wcci.FinalProjectWeCanRacquet.models;public class Set {
+}
--- frontend/js/header.js
+++ frontend/js/header.js
@@ -3,7 +3,7 @@ export default function header() {
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="/img/nav_image.png" alt="cd logo" id="navbarIcon" width="35" height="35">
+                <img src="/img/tennis.png" alt="cd logo" id="navbarIcon" width="35" height="35">
            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>

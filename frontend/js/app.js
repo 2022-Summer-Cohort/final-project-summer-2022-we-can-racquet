d82849aeb589c97f24d6864559f5d7dfedfc7f7e
tastyCanOfMalk <eddytheflow@gmail.com>
2022-08-02 11:37:59 -0400

    broken frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,4 +2,11 @@ import home from "./home.js";
 import header from "./header.js";
 import footer from "./footer.js";
 
+const container = document.querySelector(".container");
 
+function makeHomeView() {
+      container.innerHTML = header();
+
+}
+
+makeHomeView();

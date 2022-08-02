4d18505468920172b8dee4cdf0378a1a776a814e
Anish Ghimire <anishgh94@gmail.com>
2022-08-02 10:46:49 -0400

    initial setups

---
+++ backend/src/main/java/org/wcci/FinalProjectWeCanRacquet/FinalProjectWeCanRacquetApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.FinalProjectWeCanRacquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectWeCanRacquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectWeCanRacquetApplication.class, args);
+	}
+
+}

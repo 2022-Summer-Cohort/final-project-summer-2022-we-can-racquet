1066853fa15c5742363438d7f7ea034cb660410c
tastyCanOfMalk <eddytheflow@gmail.com>
2022-08-03 14:15:56 -0400

    list doesn't include player themselves

--- frontend/js/allPlayersInLeague.js
+++ frontend/js/allPlayersInLeague.js
@@ -1,8 +1,35 @@
-export default function allPlayersInLeague(player) {
-  return `
-        <div class = "allPlayersInLeague">
+export default function allPlayersInLeague(player, players) {
+  let leagueToMatch = player.league;
+  let playerName = player.name;
 
-          <h1>TEST</h1>
-        </div>
-        `;
+  return `
+
+    <div class="allPlayersInLeague">
+      <h3>Other players in ${leagueToMatch}</h3>
+      
+      <table class="table table-striped">
+      <thead>
+      <tr>
+      <th scope="col">Name</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      </tr>
+      </thead>
+      <tbody>
+      ${players.map((player) => {
+        if (player.league == leagueToMatch && player.name != playerName) {
+          return `
+                <tr>
+                  <td>${player.name}</td>
+                  <td>Challenge</td>
+                  <td>Add record</td>
+                </tr>
+              `;
+          }
+        }).join("")
+      }
+        </tbody>
+      </table>
+    </div>
+  `;
 }

7284a3241c5ca996badfc4464d9610581191f5ea
Eddie <tastyCanOfMalk@users.noreply.github.com>
2022-08-10 15:24:01 -0400

    space

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -27,7 +27,7 @@ export default function login(players) {
                 </button>
             </div>
         </div>
-
+ 
         <div class = "row">
             <div class = "d-grid gap-2 justify-content-end ">
                 <button type="button" class="mb-2 btn-lg btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
